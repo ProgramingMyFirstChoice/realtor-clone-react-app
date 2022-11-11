25ae1f8462f96d492cd18c85d526512c7966ec4f
ProgramingMyFirstChoice <34608992+ProgramingMyFirstChoice@users.noreply.github.com>
2022-11-10 19:01:10 -0800

    update the files and complete the funcionality of oauth

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
-import { Link, useNavigation } from "react-router-dom";
-import { OAuth } from "../components/OAuth";
+import { Link } from "react-router-dom";
+import OAuth from "../components/OAuth";
 import {
   getAuth,
   createUserWithEmailAndPassword,
@@ -12,29 +12,24 @@ import { doc, serverTimestamp, setDoc } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
-export const SignUp = () => {
+export default function SignUp() {
   const [showPassword, setShowPassword] = useState(false);
-
   const [formData, setFormData] = useState({
     name: "",
     email: "",
     password: "",
   });
-
   const { name, email, password } = formData;
-
   const navigate = useNavigate();
-
   function onChange(e) {
     setFormData((prevState) => ({
       ...prevState,
       [e.target.id]: e.target.value,
     }));
   }
-
   async function onSubmit(e) {
-    //prevent refreshing of the single page sit
     e.preventDefault();
+
     try {
       const auth = getAuth();
       const userCredential = await createUserWithEmailAndPassword(
@@ -42,35 +37,29 @@ export const SignUp = () => {
         email,
         password
       );
+
       updateProfile(auth.currentUser, {
         displayName: name,
       });
-
       const user = userCredential.user;
-      const formDataCopy = {
-        ...formData,
-      };
+      const formDataCopy = { ...formData };
       delete formDataCopy.password;
       formDataCopy.timestamp = serverTimestamp();
-      await setDoc(doc(db, "users", user.uid), formDataCopy);
 
-      //toast.success("Sign up was successful");
-      //navigate("/");
+      await setDoc(doc(db, "users", user.uid), formDataCopy);
+      // toast.success("Sign up was successful");
+      navigate("/");
     } catch (error) {
-      toast.error("Something went wrong to the registration");
+      toast.error("Something went wrong with the registration");
     }
   }
-
   return (
     <section>
       <h1 className="text-3xl text-center mt-6 font-bold">Sign Up</h1>
-      <div
-        className="flex justify-center flex-wrap items-center px-6 py-12
-      max-w-6xl mx-auto"
-      >
-        <div className="md:w-[67%] lg:w-[50%] mb-12 md:mb-6 ">
+      <div className="flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto">
+        <div className="md:w-[67%] lg:w-[50%] mb-12 md:mb-6">
           <img
-            src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=773&q=80"
+            src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1373&q=80"
             alt="key"
             className="w-full rounded-2xl"
           />
@@ -78,13 +67,12 @@ export const SignUp = () => {
         <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
           <form onSubmit={onSubmit}>
             <input
-              type="name"
+              type="text"
               id="name"
               value={name}
               onChange={onChange}
-              placeholder="Name"
-              className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white
-               border-gray-300 rounded translate ease-in-out"
+              placeholder="Full name"
+              className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
             />
             <input
               type="email"
@@ -92,8 +80,7 @@ export const SignUp = () => {
               value={email}
               onChange={onChange}
               placeholder="Email address"
-              className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white
-               border-gray-300 rounded translate ease-in-out"
+              className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
             />
             <div className="relative mb-6">
               <input
@@ -102,8 +89,7 @@ export const SignUp = () => {
                 value={password}
                 onChange={onChange}
                 placeholder="Password"
-                className="w-full px-4 py-2 text-xl text-gray-700 bg-white
-               border-gray-300 rounded translate ease-in-out"
+                className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
               />
               {showPassword ? (
                 <AiFillEyeInvisible
@@ -117,17 +103,12 @@ export const SignUp = () => {
                 />
               )}
             </div>
-            <div
-              className="flex justify-between
-             whitespace-nowrap text-sm sm:text-lg"
-            >
+            <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
               <p className="mb-6">
-                Have an account?
+                Have a account?
                 <Link
                   to="/sign-in"
-                  className="text-red-600 hover:text-red-800
-                transition duration-200 ease-in-out ml-1
-                "
+                  className="text-red-600 hover:text-red-700 transition duration-200 ease-in-out ml-1"
                 >
                   Sign in
                 </Link>
@@ -135,28 +116,19 @@ export const SignUp = () => {
               <p>
                 <Link
                   to="/forgot-password"
-                  className="text-blue-600 hover:text-blue-800
-                transition duration-200 ease-in-out
-                "
+                  className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out"
                 >
-                  Forgot Password?
+                  Forgot password?
                 </Link>
               </p>
             </div>
             <button
-              className="w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700
-            transition duration-150 ease-in-out hover:shadow-lg active:bg-green-800"
+              className="w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800"
               type="submit"
             >
-              Sign Up
+              Sign up
             </button>
-
-            <div
-              className=" flex items-center my-4 before:border-t
-            before:flex-1  before:border-gray-300
-            after:border-t
-            after:flex-1  after:border-gray-300"
-            >
+            <div className="flex items-center  my-4 before:border-t before:flex-1 before:border-gray-300 after:border-t after:flex-1 after:border-gray-300">
               <p className="text-center font-semibold mx-4">OR</p>
             </div>
             <OAuth />
@@ -165,4 +137,4 @@ export const SignUp = () => {
       </div>
     </section>
   );
-};
+}
